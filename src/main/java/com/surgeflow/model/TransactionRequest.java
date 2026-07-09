82f121e85242c5d22f55c14045816516db58828f
Ubuntu <azureuser@surgeflow-vm.dwjm5kb2cg4unb0mdhfryvfykd.syx.internal.cloudapp.net>
2026-07-09 08:11:47 +0000

    fix: restore all missing source files - controller, repository, kafka consumer, config

--- src/main/java/com/surgeflow/model/TransactionRequest.java
+++ src/main/java/com/surgeflow/model/TransactionRequest.java
@@ -1,8 +1,5 @@
 package com.surgeflow.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,14 +12,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransactionRequest {
-
-    @NotBlank(message = "accountId is required")
     private String accountId;
-
-    @NotNull(message = "amount is required")
-    @Positive(message = "amount must be positive")
     private BigDecimal amount;
-
-    @NotNull(message = "type is required")
     private Transaction.TransactionType type;
 }
